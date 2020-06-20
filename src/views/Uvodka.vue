1cab95134e77bcd6bf712e41d1a814d6b03762ac
LuciiR <64087763+LuciiR@users.noreply.github.com>
2020-06-20 16:00:37 +0200

    obrazky- nazev a hrat

--- src/views/Uvodka.vue
+++ src/views/Uvodka.vue
@@ -5,8 +5,8 @@
 <!--<img v-bind:src="require(`./../assets/MISTNOSTI/mistnost.png`)" alt='pozadi'> -->
 
 
-<img id = "nazev" v-bind:src="require(`./../assets/nazev.png`)" alt='nazev'> 
-<img id= "hrat" v-bind:src="require(`./../assets/tlacitko.png`)" alt='tlacitko'> 
+<img class="nazev" v-bind:src="require(`./../assets/nazev.png`)" alt='nazev'> 
+<img class="hrat" v-bind:src="require(`./../assets/tlacitko.png`)" alt='tlacitko'> 
 
 
 </div>
@@ -22,20 +22,26 @@ export default {
 
 .nazev {
     position: absolute;
+     left:340px;
+     top: 150px;
+    width: 429px;
+    height: 250px;
     
 
 }
 
 .hrat {
    position: absolute;
-   top: 200;
+   top: 580px;
+   left:430px;
+   
     
 
 }
 
 .pozadi {
     position: relative;
-    left:50px;
+    left:130px;
     background-image:url("./../assets/MISTNOSTI/mistnost.png");
     background-repeat: no-repeat;
     width: 1080px;
