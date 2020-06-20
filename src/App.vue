7c3713068b549ca90f2c4710e2bf71e235240619
LuciiR <64087763+LuciiR@users.noreply.github.com>
2020-06-20 15:37:46 +0200

    Uvodka

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <router-link to ="/uvodka"> Uvod do hry </router-link>
     </div>
     <router-view/>
   </div>
---
+++ src/views/Uvodka.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div class = "pozadi">
+
+<!--<img v-bind:src="require(`./../assets/MISTNOSTI/mistnost.png`)" alt='pozadi'> -->
+
+
+<img id = "nazev" v-bind:src="require(`./../assets/nazev.png`)" alt='nazev'> 
+<img id= "hrat" v-bind:src="require(`./../assets/tlacitko.png`)" alt='tlacitko'> 
+
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+.nazev {
+    position: absolute;
+    
+
+}
+
+.hrat {
+   position: absolute;
+   top: 200;
+    
+
+}
+
+.pozadi {
+    position: relative;
+    left:50px;
+    background-image:url("./../assets/MISTNOSTI/mistnost.png");
+    background-repeat: no-repeat;
+    width: 1080px;
+    height: 720px;
+}
+
+
+</style>
